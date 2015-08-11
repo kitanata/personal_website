7b57348cfc4db9aa070df629cf1c49bc2d46e8e0
Raymond Chandler III <raymondchandleriii@gmail.com>
2015-08-10 20:16:45 -0400

    WIP

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Raymond Chandler III'
+SITENAME = 'Raymond Chandler III'
+SITEURL = ''
+TWITTER_USERNAME = '@raymondchandler'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Indiana/Indianapolis'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Collidr', 'http://www.collidr.io'),
+        ('Corrupted Kingdoms', 'https://boardgamegeek.com/boardgame/168895/corrupted-kingdoms'),
+        ('PyOhio', 'http://www.pyohio.org/'),
+        ('Columbus Python', 'http://www.cohpy.org/'),
+        ('Columbus Rust Society', 'http://www.meetup.com/columbus-rs/'),
+        ('Columbus Ruby Brigade', 'http://columbusrb.com/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/raymondchandler'),
+          ('Github', 'https://github.com/kitanata'),
+          ('LinkedIn', 'https://www.linkedin.com/in/kitanata'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
